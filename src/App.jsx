b8a6960c2df9c8bfe3431d2bd37c3106febcb5bd
Zeljko Damnjanovic <z.damnjanovic@emakina.rs>
2022-11-25 07:33:56 +0100

    Bootstrap, Bootstrap-icons and Scss setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+	return (
+		<div className='container mt-5'>
+			<h1 className='text-primary'>
+				<i className='bi bi-diagram-2-fill'></i>Report
+			</h1>
+		</div>
+	);
+}
